2744658b18ccd59b19c236cc39aa942859dba80b
Davert <davert.ua@gmail.com>
2020-03-20 21:52:28 +0100

    added github actions

---
+++ test/codecept.puppeteer.conf.js
@@ -0,0 +1,29 @@
+
+module.exports.config = {
+  tests: './*_test.js',
+  timeout: 10000,
+  output: './output',
+  helpers: {
+    Puppeteer: {
+      url: 'http://localhost:8000',
+      show: true,
+      chrome: {
+        args: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+        ],
+      },
+    },
+    MockRequestHelper: {
+      require: '../index.js'
+    },
+  },
+  include: {},
+  bootstrap: false,
+  mocha: {},
+  name: 'acceptance',
+  gherkin: {
+    features: './gherkin/*.feature',
+    steps: ['./gherkin/steps.js'],
+  },
+};
---
+++ test/codecept.webdriver.conf.js
@@ -0,0 +1,24 @@
+module.exports.config = {
+  tests: './*_test.js',
+  timeout: 10000,
+  output: './output',
+  helpers: {
+    WebDriver: {
+      url: 'http://localhost:8000',
+      browser: 'chrome',
+      // host: TestHelper.seleniumHost(),
+      // port: TestHelper.seleniumPort(),
+      // disableScreenshots: true,
+      // desiredCapabilities: {
+      //   chromeOptions: {
+      //     args: ['--headless', '--disable-gpu', '--window-size=1280,1024'],
+      //   },
+      // },
+    },
+    MockRequest: {},
+  },
+  include: {},
+  bootstrap: done => setTimeout(done, 5000), // let's wait for selenium
+  mocha: {},
+  name: 'acceptance',
+};
